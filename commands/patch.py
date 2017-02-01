4afb2cfd514663d8db1a9e3ab44ee27e61d7f23d
Serge Rehbinder <serge.rehbinder@cea.fr>
2017-02-01 14:40:03 +0100

    'sat patch': use the system command patch instead of the pyton command patching.py

--- commands/patch.py
+++ commands/patch.py
@@ -80,10 +80,8 @@ def apply_patch(config, product_info, max_product_name_len, logger):
         
         # Check the existence and apply the patch
         if os.path.isfile(patch):
-            #patch_exe = "patch" # old patch command (now replace by patching.py)
-            patch_exe = os.path.join(config.VARS.srcDir, "patching.py")
-            patch_cmd = "python %s -p1 -- < %s" % (patch_exe, patch)
-
+            patch_cmd = "patch -p1 < %s" % patch
+            
             # Write the command in the terminal if verbose level is at 5
             logger.write(("    >%s\n" % patch_cmd),5)
             
