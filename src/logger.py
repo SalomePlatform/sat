2a6a0fc5ab906cbf6a213a4e2caf36c4a818a0c9
SPN Salome - CEA <support-salome@cea.fr>
2017-06-07 16:19:03 +0200

    bug fix for the jobs command

--- src/logger.py
+++ src/logger.py
@@ -87,12 +87,12 @@ class Logger(object):
         try:
             self.logTxtFile = open(str(self.txtFilePath), 'w')
         except IOError:
-            msg1 = _("WARNING! Trying to write to a file that"
-                     " is not accessible:")
-            msg2 = _("The logs won't be written.")
-            print("%s\n%s\n%s\n" % (src.printcolors.printcWarning(msg1),
-                                    src.printcolors.printcLabel(str(self.txtFilePath)),
-                                    src.printcolors.printcWarning(msg2) ))
+            #msg1 = _("WARNING! Trying to write to a file that"
+            #         " is not accessible:")
+            #msg2 = _("The logs won't be written.")
+            #print("%s\n%s\n%s\n" % (src.printcolors.printcWarning(msg1),
+            #                        src.printcolors.printcLabel(str(self.txtFilePath)),
+            #                        src.printcolors.printcWarning(msg2) ))
             self.logTxtFile = tempfile.TemporaryFile()
             
         # If the option all_in_terminal was called, all the system commands
